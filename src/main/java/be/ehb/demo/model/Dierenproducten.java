07079312e1abb4c01581276374134d642c4a0ca1
mobapp13 <taliadecremer@telenet.be>
2019-12-16 16:28:12 +0100

    klasse Klant aangemaakt

--- src/main/java/be/ehb/demo/model/Dierenproducten.java
+++ src/main/java/be/ehb/demo/model/Dierenproducten.java
@@ -16,13 +16,7 @@ public class Dierenproducten {
     public String productNaam;
     public String productOmschrijving;
     public BigDecimal prijs;
-    public int aantal;
-    public BigDecimal totaal;
-    public String email;
-    public String klantNaam;
-    public String adres;
-    public String woonplaats;
-    public int postcode;
+
 
     public Dierenproducten() {
     }
@@ -58,61 +52,4 @@ public class Dierenproducten {
     public void setPrijs(BigDecimal prijs) {
         this.prijs = prijs;
     }
-
-    public int getAantal() {
-        return aantal;
-    }
-
-    public void setAantal(int aantal) {
-        this.aantal = aantal;
-    }
-
-    public BigDecimal getTotaal() {
-        return totaal;
-    }
-
-    public void setTotaal(BigDecimal totaal) {
-        this.totaal = totaal;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getKlantNaam() {
-        return klantNaam;
-    }
-
-    public void setKlantNaam(String klantNaam) {
-        this.klantNaam = klantNaam;
-    }
-
-    public String getAdres() {
-        return adres;
-    }
-
-    public void setAdres(String adres) {
-        this.adres = adres;
-    }
-
-    public String getWoonplaats() {
-        return woonplaats;
-    }
-
-    public void setWoonplaats(String woonplaats) {
-        this.woonplaats = woonplaats;
-    }
-
-    public int getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(int postcode) {
-        this.postcode = postcode;
-    }
-
 }
