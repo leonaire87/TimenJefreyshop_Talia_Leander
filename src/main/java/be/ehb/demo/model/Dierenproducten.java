f6b5d7718b5caa11426fb57d08866f288e560cc0
leonaire87 <leander_catteeuw@hotmail.com>
2019-12-16 14:38:37 +0100

    Initial commit

---
+++ src/main/java/be/ehb/demo/model/Dierenproducten.java
@@ -0,0 +1,7 @@
+package be.ehb.demo.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Dierenproducten {
+}
---
+++ src/main/java/be/ehb/demo/model/DierenproductenDAO.java
@@ -0,0 +1,4 @@
+package be.ehb.demo.model;
+
+public interface DierenproductenDAO {
+}
