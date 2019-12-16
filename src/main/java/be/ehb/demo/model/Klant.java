b9dc0cbc51058949042513d88f36f76e3e7df93f
mobapp13 <taliadecremer@telenet.be>
2019-12-16 16:29:54 +0100

    klasse Klant aangemaakt

---
+++ src/main/java/be/ehb/demo/model/Klant.java
@@ -0,0 +1,71 @@
+package be.ehb.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Klant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    public String email;
+    public String klantNaam;
+    public String adres;
+    public String woonplaats;
+    public int postcode;
+
+    public Klant() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKlantNaam() {
+        return klantNaam;
+    }
+
+    public void setKlantNaam(String klantNaam) {
+        this.klantNaam = klantNaam;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+}
